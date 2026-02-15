5303c4bed6ce19468211cf1a1573d43c99e46d62
grahamsan <rbirisso@gmail.com>
2026-02-15 02:37:13 +0100

    fixed deployment bug

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -8,10 +8,18 @@ import { User } from '../users/entities/user.entity';
 import { Activity } from '../activity/activity.entity';
 import { Vehicle } from '../vehicles/vehicle.entity';
 import { ConversationsModule } from '../conversations/conversations.module';
+import { Place } from '../places/place.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Trip, TripApplication, User, Activity, Vehicle]),
+    TypeOrmModule.forFeature([
+      Trip,
+      TripApplication,
+      User,
+      Activity,
+      Vehicle,
+      Place,
+    ]),
     ConversationsModule,
   ],
   controllers: [TripsController],
